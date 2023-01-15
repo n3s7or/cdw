bf8ef2f56d1fd21f378734359ff5e27581241760
n3s7or <nrmorales.93@gmail.com>
2023-01-15 16:39:48 -0500

    Added command `builds` which will handle any action related ti builds
    * added command `builds`, refactor to reuse common code (#8)
    fixes #4

--- cdw/main.go
+++ cdw/main.go
@@ -4,25 +4,23 @@ import (
 	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
 
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/AlecAivazis/survey/v2/terminal"
 	"github.com/aws/aws-sdk-go-v2/config"
-	cbtypes "github.com/aws/aws-sdk-go-v2/service/codebuild/types"
-	"github.com/gen2brain/beeep"
+	"github.com/n3s7or/cdw/pkg/commands"
+	"github.com/n3s7or/cdw/pkg/common"
 	"github.com/n3s7or/cdw/pkg/naws"
-    "github.com/n3s7or/cdw/pkg/commands"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
     app := &cli.App{
-        Name:                 "cdw",
-		Usage:                "https://github.com/n3s7or/cdw#readme",
-        Commands:   []*cli.Command{&commands.ProjectsCommand}, // todo: add commands
-        Action: func(c *cli.Context) error {
+        Name:                   "cdw",
+		Usage:                  "https://github.com/n3s7or/cdw#readme",
+        Commands:               []*cli.Command{&commands.ProjectsCommand, &commands.BuildsCommand},
+        EnableBashCompletion:   true,
+        Action: func(c *cli.Context) error {        // todo: when a non command argument is passed, show cli usage help
 
             cfg, err := config.LoadDefaultConfig(c.Context)
             if err != nil {
@@ -30,10 +28,8 @@ func main() {
             	return err
             }
 
-            cbClient := naws.GetClient(c, &cfg)
-
             var cbNextToken *string // ToDo: get other projects if there is a next token
-            projects, err := naws.ListProjects(c, cbClient, cbNextToken)
+            projects, err := naws.ListProjects(c, &cfg, cbNextToken)
             if err != nil {
                 log.Fatal(err.Error())
                 return err
@@ -51,7 +47,7 @@ func main() {
                 return err
             }
 
-            buildsListNames, err := naws.ListBuilds(c, cbClient, &projects[selectedIndex])
+            buildsListNames, err := naws.ListBuilds(c, &cfg, &projects[selectedIndex])
             if err != nil {
                 println(err.Error())
                 return nil
@@ -62,26 +58,13 @@ func main() {
                 return nil
             }
 
-            var builds []cbtypes.Build
-            if len(buildsListNames) > 9 {
-                builds, err = naws.GetBuildsInfo(c, cbClient, buildsListNames[0:10])
-            }else {
-                builds, err = naws.GetBuildsInfo(c, cbClient, buildsListNames)
-            }
+            builds, err := common.GetBuildsFromBuildsName(c, cfg, buildsListNames)
             if err != nil {
                 log.Fatal(err.Error())
-                return nil
             }
             
-            var listBuilds []string;
-            for _, item := range(builds){
-                id := string(strings.Split((*item.Id), ":")[1])
-                initiator := string(*item.Initiator)
-                opt := fmt.Sprintf("%-*s\t%s\t(%s)", 11, fmt.Sprintf("[%s]", string(item.BuildStatus)), initiator, id)
-                listBuilds = append(listBuilds, opt)
-            }
-
-            in = survey.Select{Message: "Select build:", Options: listBuilds}
+            surveyBuildsOptions := common.GenerateOptionsListForsurvey(builds)
+            in = survey.Select{Message: "Select build:", Options: surveyBuildsOptions}
             err = survey.AskOne(&in, &selectedIndex)
             if err != nil {
                 if err == terminal.InterruptErr {
@@ -92,58 +75,8 @@ func main() {
                 return err
             }
 
-            selectedBuild := builds[selectedIndex]
+            common.MonitorBuild(c, &cfg, &builds[selectedIndex])
 
-            var group *string
-            var stream *string
-            
-            // sometimes when getting log config so quickly it returns empty group or stream
-            for _, d := range([]time.Duration{350, 650, 1000}) {
-                group = selectedBuild.Logs.GroupName
-                stream = selectedBuild.Logs.StreamName
-
-                if group != nil && stream != nil { break }
-
-                time.Sleep(time.Millisecond * d)
-                build, err := naws.GetBuildInfo(c, cbClient, buildsListNames[selectedIndex])
-                if err != nil {
-                    log.Fatal(err.Error())
-                    return nil
-                }
-                selectedBuild = build
-                selectedBuild = build
-            }
-
-            var token *string
-            for  {
-                res, err := naws.GetEvents(c, &cfg, group, stream, token)
-                if err != nil {
-                    log.Fatal(err.Error())
-                    return err
-                }
-
-                for _, event := range(res.Events) {
-                    fmt.Print(*event.Message)
-                }
-
-                // check current build state (keeping selectedBuild value)
-                build, err := naws.GetBuildInfo(c, cbClient, *selectedBuild.Id)
-                if err != nil{
-                    log.Fatal(err.Error())
-                    return err
-                }
-
-                if (token != nil && *token == *res.NextForwardToken && build.BuildComplete) {
-                    if !selectedBuild.BuildComplete {
-                        beeep.Alert("CodeWatch", "Build completed", "")
-                    }
-                    break;
-                }
-
-                token = res.NextForwardToken
-                time.Sleep(2*time.Second)
-            }
-            
             return nil
         },
     }
---
+++ pkg/commands/builds.go
@@ -0,0 +1,70 @@
+package commands
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/AlecAivazis/survey/v2/terminal"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/n3s7or/cdw/pkg/common"
+	"github.com/n3s7or/cdw/pkg/naws"
+	"github.com/urfave/cli/v2"
+)
+
+var BuildsCommand = cli.Command{
+	Name: "builds",
+	Usage: "List builds for a provided project",
+	Flags: []cli.Flag{
+		&cli.StringFlag{Name: "project", Required: true, Usage: "project name to check builds"},
+		&cli.BoolFlag{Name: "prompt", Usage: "If provided, prompts user to select one build among the last 10 builds, show last build logs otherwise"},
+	},
+	Action: func(ctx *cli.Context) error {
+
+		prompt := ctx.Bool("prompt")
+
+		project := ctx.String("project")
+		if project == ""{
+			log.Fatal("no project provided")
+		}
+
+		cfg, err := config.LoadDefaultConfig(ctx.Context)
+		if err != nil {
+			log.Fatal(err.Error())
+			return err
+		}		
+		
+		buildsName, err := naws.ListBuilds(ctx, &cfg, &project)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		if len(buildsName) == 0 {
+			fmt.Printf("No builds available for project `%s`\n", project)
+			return nil
+		}
+
+		builds, err := common.GetBuildsFromBuildsName(ctx, cfg, buildsName)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+
+		selectedIndex := 0
+		if prompt {
+			surveyOptions := common.GenerateOptionsListForsurvey(builds)
+			in := survey.Select{Message: "Select build:", Options: surveyOptions}
+			
+			err = survey.AskOne(&in, &selectedIndex)
+			if err != nil {
+				if err == terminal.InterruptErr {
+					fmt.Println("Interrupted by user, exiting...")
+					return nil
+				}
+				log.Fatal(err.Error())
+			}
+		}
+		
+		common.MonitorBuild(ctx, &cfg, &builds[selectedIndex])
+
+		return nil
+	},
+}
